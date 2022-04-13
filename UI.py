d525cc555a7371327a2576dd005840bab13ccaf9
TheTiagoMarabuto <up201604598@fe.up.pt>
2022-04-13 17:51:56 +0100

    first GUI Files

---
+++ UI.py
@@ -0,0 +1,46 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QTextEdit, QPushButton, QFileDialog, QMainWindow
+from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from window import Ui_window
+
+
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+
+        # Load the ui file
+        uic.loadUi("/Users/tiagomarabuto/PycharmProjects/SAD3E/mainwindow.ui", self)
+
+        # Load second window
+        self.window = QMainWindow()
+        self.ui = Ui_window()
+
+        # Define Our Widgets
+        self.button = self.findChild(QPushButton, "new_buttom")
+
+        # Open Dialog box and get file path
+        self.button.clicked.connect(self.clicker)
+
+        # Show The App
+        self.show()
+
+    def clicker(self):
+        fname = QFileDialog.getOpenFileName(self, "Open File", "", "PNG (*.png);;JPEG (*.jpeg) ")[0]
+        self.openWindow(fname)
+
+    def openWindow(self, filename):
+
+        self.ui.setupUi(self.window)
+        # Open image
+        pixmap = QPixmap(filename)
+        #adjusted_pixmap = pixmap.scaled(900, 900, Qt.KeepAspectRatio)
+        self.ui.image.setPixmap(pixmap)
+        self.window.show()
+
+
+# Initialize The App
+app = QApplication(sys.argv)
+UIWindow = UI()
+app.exec_()
